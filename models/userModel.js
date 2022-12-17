5ba70b06d5b907f6a67519d232160b990cd55424
Atharva Parkale <atharva.parkale@gmail.com>
2022-12-17 11:31:07 +0530

    second commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,16 +23,33 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password !'],
-    validate:{
-        validator:function(el){
-            return el===this.password
-        }
-    }
+
+    //This only works on save
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
+    message: 'Passwords are not the same !',
   },
 });
 
+//BETWEEN THE MOMENT WE RECEIVE THE DATA AND SAVE INTO THE DATABASE
+//IT IS A MIDDLEWARE
+
+userSchema.pre('save', async function (next) {
+
+  //ONLY RUN THIS FUNCTION IF PASSWORD WAS ACTUALLY MODIFY
+  if (!this.isModified('password')) return next();
+  
+  //HASH THE PASSWORD WITH COST OF 12
+  this.password = await bcrypt.hash(this.password, 12);
+  
+  //DELETE PASSWORDCONFIRM FIELD
+  this.passwordConfirm = undefined;
+  next();
+});
+
 //CREATING MODEL OUT OF THE SCHEMA
-
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
